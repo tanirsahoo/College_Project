dbdc98e05d3f1600465ce04e9a57fda692dc72c2
tanirsahoo <tanir.sahoo.2021@gmail.com>
2024-09-11 10:11:51 +0530

    Updates

---
+++ Rental_Project_Work/src/test/java/Rental/example/Rental_Project_Work/RentalProjectWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package Rental.example.Rental_Project_Work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalProjectWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
